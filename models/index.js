9f2b907ceea4cea162dfbbe475c82409b83b79f4
Darren <you@example.comdarren@kandekore.net>
2022-11-06 21:37:31 +0000

    models

---
+++ models/index.js
@@ -0,0 +1,26 @@
+const User = require("./User");
+const Post = require("./Post.js");
+const Categories = require("./Categories");
+const Comments = require("./Comments");
+
+User.hasMany(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+User.hasMany(Comments, {
+  foreignKey: "comment_id",
+  onDelete: "CASCADE",
+});
+
+Post.belongsTo(User, {
+  foreignKey: "user_id",
+  onDelete: "CASCADE",
+});
+
+Categories.hasMany(Post, {
+  foreignKey: "post_id",
+  onDelete: "CASCADE",
+});
+
+module.exports = { User, Post, Categories, Comments };
